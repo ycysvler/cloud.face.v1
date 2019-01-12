d7f7b0fa3b10e9fc5e199c63670865b540b5fbe1
VLER <ycysvler@hotmail.com>
2019-01-12 15:56:52 +0800

    * modify test

--- server/service/test.py
+++ server/service/test.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     model_dir = "/root/faceRetrieval/models"
     detector = IFaceZoneDetect(model_dir, 0)
     pic_dir = "/root/pic/"
-    picname = pic_dir + "102_0_783-160-856-233_5329.jpg"
+    picname = "102_0_783-160-856-233_5329.jpg"
     im = cv2.imread(pic_dir + picname)
     boxes, points = detector.detect(im)
     print 'face service > boxes >', '\033[1;32m request  ' + str(len(boxes)) + ' \033[0m'
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
     if (len(boxes) == len(points)):
         im_temp = IFaceZoneDetect.get_align_face(detector, im, boxes[0], points[0])
-        cv2.write("/root/001.jpg", im_temp)
+        cv2.imwrite("/root/002.jpg", im_temp)
 
 
 
