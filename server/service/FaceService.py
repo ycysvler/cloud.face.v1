998b3065d0fd6710f29fd05cead1230c2d3da7aa
VLER <ycysvler@hotmail.com>
2019-01-12 23:41:45 +0800

    * modify test

--- server/service/FaceService.py
+++ server/service/FaceService.py
@@ -34,37 +34,64 @@ app = Flask(__name__)
 
 pip_app, pip_service = Pipe()
 
+def singleFeature(params):
+    return {"code":200, "face_id":params["face_id"]}
+
+def batchFeature(params):
+    return {"code": 200, "group_id": params["group_id"]}
+
+def buildIndex(params):
+    return {"code": 200, "group_id": params["group_id"]}
+
+def query(params):
+    return {"code": 200, "group_id": params["group_id"]}
+
+# 後臺服務
 def startService():
     print 'face service > ', '\033[1;32m ' + 'started !' + ' \033[0m'
     while True:
         params = pip_service.recv()
         print 'face service > work >', '\033[1;32m request  ' + str(params) + ' \033[0m'
-        pip_service.send({"code": 200 , "id":params["id"]})
+        if params["type"] == "singlefeature":
+            result = singleFeature(params)
+        if params["type"] == "batchfeature":
+            result = singleFeature(params)
+        if params["type"] == "buildindex":
+            result = buildIndex(params)
+        if params["type"] == "query":
+            result = query(params)
+        pip_service.send(result)
 
 # 单图像计算特征
 @app.route('/singlefeature')
-def singleFeature():
-    id = request.args.get("face_id")
-    return Response(json.dumps({"id":id, "haha":"haha"}),mimetype='application/json')
+def resetSingleFeature():
+    face_id = request.args.get("face_id")
+    pip_app.send({"type": "singlefeature", "face_id": face_id})
+    result = pip_app.recv()
+    return Response(json.dumps(result), mimetype='application/json')
 
 # 多图像计算特征
 @app.route('/batchfeature')
-def batchFeature():
-    id = request.args.get("group_id")
-    return Response(json.dumps({"id":id, "haha":"haha"}),mimetype='application/json')
+def restBatchFeature():
+    group_id = request.args.get("group_id")
+    pip_app.send({"type": "batchfeature", "group_id": group_id})
+    result = pip_app.recv()
+    return Response(json.dumps(result), mimetype='application/json')
 
 # 人像库构建索引
 @app.route('/buildindex')
-def buildIndex():
-    id = request.args.get("group_id")
-    return Response(json.dumps({"id":id, "haha":"haha"}),mimetype='application/json')
+def restBuildIndex():
+    group_id = request.args.get("group_id")
+    pip_app.send({"type": "buildindex", "group_id": group_id})
+    result = pip_app.recv()
+    return Response(json.dumps(result),mimetype='application/json')
 
 # 人像库搜索人像
 @app.route('/query')
-def query():
-    id = request.values.get("group_id")
+def restQuery():
+    group_id = request.values.get("group_id")
     image_path = request.values.get("image_path")
-    pip_app.send({"id": id})
+    pip_app.send({"type":"query", "group_id": group_id, "image_path":image_path})
     result = pip_app.recv()
     return Response(json.dumps(result),mimetype='application/json')
 
