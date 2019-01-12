624e72cc259ae2084d1c1cd1f2b903d44ef600cf
VLER <ycysvler@hotmail.com>
2019-01-12 22:46:46 +0800

    * modify test

---
+++ server/service/FaceService.py
@@ -0,0 +1,64 @@
+#coding=utf-8
+# -*- coding: UTF-8 -*-
+import os
+import sys
+import cv2
+import time
+import json
+import config
+import urllib
+import mongodb
+import argparse
+import datetime
+import bson.binary
+
+from bson.objectid import ObjectId
+from IFaceDetect import IFaceZoneDetect
+from IFaceRetrieval import FaceRetrieval
+from multiprocessing import Process, Pipe
+from flask import Flask,request ,Response
+
+# 扩展加载路径
+sys.path.append("./dll")
+sys.path.append("/root/faceRetrieval")
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+# http server
+app = Flask(__name__)
+
+# 单图像计算特征
+@app.route('/singlefeature')
+def singleFeature():
+    id = request.args.get("face_id")
+    return Response(json.dumps({"id":id, "haha":"haha"}),mimetype='application/json')
+
+# 多图像计算特征
+@app.route('/batchfeature')
+def batchFeature():
+    id = request.args.get("group_id")
+    return Response(json.dumps({"id":id, "haha":"haha"}),mimetype='application/json')
+
+# 人像库构建索引
+@app.route('/buildindex')
+def buildIndex():
+    id = request.args.get("group_id")
+    return Response(json.dumps({"id":id, "haha":"haha"}),mimetype='application/json')
+
+# 人像库搜索人像
+@app.route('/query')
+def query():
+    id = request.args.get("group_id")
+    image_path = request.args.get("image_path")
+    return Response(json.dumps({"id":id,"image_path":image_path, "haha":"haha"}),mimetype='application/json')
+
+# http server progress, port = 4003
+def webService():
+    app.run(debug=False, host='0.0.0.0', port=4003)
+
+# main
+if __name__ == '__main__':
+# 启动计算集成等待努力工作
+    webProcess = Process(target=webService)
+    webProcess.start()
