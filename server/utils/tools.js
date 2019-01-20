b799921557b88e55bcf6c6a567df50c815abe1e8
VLER <ycysvler@hotmail.com>
2019-01-20 23:41:40 +0800

    * add face 、 group 、user

--- server/utils/tools.js
+++ server/utils/tools.js
@@ -15,7 +15,7 @@ class tools {
     * 获取本机IP地址
     * */
     getIps(){
-        return "127.0.0.1";
+        //return "127.0.0.1";
 
         let IPv4s = [];
         let en0 = os.networkInterfaces().en0 ? os.networkInterfaces().en0:os.networkInterfaces().eth0;
